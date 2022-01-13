42ab959bb21b638b559b1ee45849dd2dd62eeffe
italolelis <italolelis@gmail.com>
2022-01-13 14:42:34 +0100

    Update install

--- .zshrc
+++ .zshrc
@@ -9,18 +9,23 @@ unset file;
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
-# # Path to your oh-my-zsh installation.
+# Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
-
-plugins=(git)
-
-source $ZSH/oh-my-zsh.sh
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 ZSH_THEME=""
+
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git gh)
+
+source $ZSH/oh-my-zsh.sh
 
 if [ -z "$SSH_AUTH_SOCK" ]; then
    # Check for a currently running instance of the agent
--- install.sh
+++ install.sh
@@ -7,39 +7,20 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "install.sh" \
-		--exclude "README.md" \
-		-avh --no-perms . ~;
-	source ~/.zshrc;
-}
+rsync --exclude ".git/" \
+	--exclude ".DS_Store" \
+	--exclude ".osx" \
+	--exclude "install.sh" \
+	--exclude "README.md" \
+	-avh --no-perms . ~;
 
-function ohMyZsh() {
-	oh_my_install_dir="$HOME/.oh-my-zsh"
-	user_rc_file="$HOME/.zshrc"
+source ~/.zshrc;
 
-	sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-	powerlevel10k_dir="${oh_my_install_dir}/.oh-my-zsh/custom/themes/powerlevel10k"
-	if [ ! -d "$oh_my_install_dir" ]; then
-		git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${powerlevel10k_dir}
-		sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="powerlevel10k/powerlevel10k"/g' ${user_rc_file}
-	fi
-}
-
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	ohMyZsh;
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		ohMyZsh;
-		doIt;
-	fi;
-fi;
-unset ohMyZsh;
-unset doIt;
+user_rc_file="$HOME/.zshrc"
+powerlevel10k_dir="${$ZSH}/custom/themes/powerlevel10k"
+if [ ! -d "$powerlevel10k_dir" ]; then
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${powerlevel10k_dir}
+	sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="powerlevel10k/powerlevel10k"/g' ${user_rc_file}
+fi
