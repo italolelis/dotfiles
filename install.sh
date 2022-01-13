18f2cbb1acdf9002dcb038fa2d08788a24be8cfa
italolelis <italolelis@gmail.com>
2022-01-13 15:15:08 +0100

    Fix typo

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ rsync --exclude ".git/" \
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 user_rc_file="$HOME/.zshrc"
-powerlevel10k_dir="${$ZSH}/custom/themes/powerlevel10k"
+powerlevel10k_dir="${ZSH}/custom/themes/powerlevel10k"
 if [ ! -d "$powerlevel10k_dir" ]; then
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${powerlevel10k_dir}
 	sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="powerlevel10k/powerlevel10k"/g' ${user_rc_file}
